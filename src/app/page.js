a4fbb30752ffc57bbb8a649cbf8ed7c46d6caade
Luzuko Naki <108240690+Lunakill@users.noreply.github.com>
2024-07-25 12:31:33 +0200

    rightparticles

--- src/app/page.js
+++ src/app/page.js
@@ -17,9 +17,9 @@ export default function Home() {
         <AboutSection />
         <ProjectsSection />
         <EmailSection />
-        <ParticlesComponent id="particles" />
       </div>
       <Footer />
+        <ParticlesComponent id="particles" />
     </main>
   );
 }
