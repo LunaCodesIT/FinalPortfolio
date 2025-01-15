a83668fd5be33480ec228ad8269813afdec0a0b3
Luzuko Naki <108240690+Lunakill@users.noreply.github.com>
2025-01-16 00:03:52 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import ParticlesComponent from "../app/components/particles";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#000000]">
+    <main className="flex min-h-screen flex-col bg-[##404040]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
